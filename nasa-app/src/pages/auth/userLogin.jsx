afa39e11cdf03807eafa073e103c0e1ea86032d5
Hansana K.T <99861562+it21167850@users.noreply.github.com>
2024-05-05 01:37:29 +0530

    Update userLogin.jsx

--- nasa-app/src/pages/auth/userLogin.jsx
+++ nasa-app/src/pages/auth/userLogin.jsx
@@ -27,7 +27,7 @@ function UserLogin({ setIsLoggedIn }) {
   const handleLogin = async () => {
     try {
       const response = await axios.post(
-        "https://nasa-app-api-one.vercel.app/api/v1/users/login",
+        "/api/v1/users/login",
         { email, password }
       );
       console.log("Login successful:", response);
