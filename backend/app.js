77124ecc1a2b3e618d8be738e541f60571de76a7
Hansana K.T <99861562+it21167850@users.noreply.github.com>
2024-05-05 09:03:12 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -16,7 +16,7 @@ const app = express();
 // Implement CORS: Allow CORS for all incoming requests to our API.
 app.use(
   cors({
-    origin: "https://nasa-app-chi.vercel.app",
+    origin: "*",
     methods: ["GET", "POST"],
     credentials: true,
   })
