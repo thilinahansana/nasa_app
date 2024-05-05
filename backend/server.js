6fb2d78369b91dc90d2363c2d9096f5ad5a8b1ff
Hansana K.T <99861562+it21167850@users.noreply.github.com>
2024-05-05 11:29:58 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,38 +1,34 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
-// Handle uncaught exceptions globally
 process.on("uncaughtException", (err) => {
   console.error("UNCAUGHT EXCEPTION! 💥 Shutting down...");
   console.error(err.name, err.message);
   process.exit(1);
 });
-
-// Load environment configurations
+//Load Environment Configs
 dotenv.config({ path: "./config.env" });
 
-// Require app.js
+//require app.js
 const app = require("./app");
 
-// Database Connection
+//Database Connection
 const DB = process.env.DATABASE_URI;
 
-mongoose.connect(DB, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false
-})
-.then(() => console.log("DB connection successful!"))
-.catch((err) => {
-  console.error("Error connecting to DB:", err.message);
-  process.exit(1);
-});
+mongoose.set("strictQuery", false);
 
-// Specify the port to listen on
+mongoose
+  .connect(DB)
+  .then(() => console.log("DB connection successful!"))
+  .catch((err) => {
+    console.error("Error connecting to DB:", err.message);
+    process.exit(1);
+  });
+
+// Run this system on port 3000
 const port = process.env.PORT || 8000;
 
-// Start the server
+//start server
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
