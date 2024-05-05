0be078f7666ecf6c6a96853565dbbc10627e40e1
Hansana K.T <99861562+it21167850@users.noreply.github.com>
2024-05-05 09:50:47 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,9 @@
-const path = require("path");
 const express = require("express");
-const morgan = require("morgan");
-const helmet = require("helmet");
 const cors = require("cors");
-const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
+const helmet = require("helmet");
 const mongoSanitize = require("express-mongo-sanitize");
 const xss = require("xss-clean");
 const AppError = require("./utils/appError");
-
 const userRouter = require("./Routes/userRouter");
 
 const app = express();
@@ -22,22 +17,15 @@ app.use(
   })
 );
 
-app.options("*", cors()); //all resources
-
 // Set security HTTP headers
 app.use(helmet({ contentSecurityPolicy: false }));
-
-// Development logging
-if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"));
-}
 
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: "10kb" }));
 app.use(express.urlencoded({ extended: true, limit: "10kb" }));
 
 // app.use(cookieParser()) is setting up a middleware that will parse incoming request
-//headers for cookies and make them available on the request object.
+// headers for cookies and make them available on the request object.
 app.use(cookieParser());
 
 // Data sanitization against NoSQL query injection
@@ -46,13 +34,10 @@ app.use(mongoSanitize());
 // Data sanitization against XSS
 app.use(xss());
 
-export default function handler(request, response) {
-  const { name = 'World' } = request.query;
-  return response.send(`Hello ${name}!`);
-}
-// 3) ROUTES
+// ROUTES
 app.use("/api/v1/users", userRouter);
 
+// Handle undefined routes
 app.all("*", (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
